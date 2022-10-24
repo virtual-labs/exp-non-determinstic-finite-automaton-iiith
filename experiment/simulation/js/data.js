e7b8598f4f51b0540f85dffca7cfae5e0594a2f6
nithish <nithish98raja@gmail.com>
2022-10-24 13:49:52 +0530

    update NFA data

--- experiment/simulation/js/data.js
+++ experiment/simulation/js/data.js
@@ -1,29 +1,30 @@
 
-
-// NFA to accept even numbers
 const nfa1 = {
   "vertices": [
     {"text": "A", "type": "start"},
-    {"text": "B", "type": "accept"},
+    {"text": "B", "type": "none"},
+    {"text": "C", "type": "none"},
+    {"text": "D", "type": "accept"}
   ],
   "edges": [
-    {"start": "A", "end": "A", "text": "1", "type": "self"},
-    {"start": "A", "end": "B", "text": "0", "type": "forward"},
-    {"start": "B", "end": "A", "text": "1", "type": "backward"},
-    {"start": "B", "end": "B", "text": "0", "type": "self"}
+    {"start": "A", "end": "A", "text": "0,1", "type": "self"},
+    {"start": "A", "end": "B", "text": "1", "type": "forward"},
+    {"start": "B", "end": "C", "text": "0", "type": "forward"},
+    {"start": "C", "end": "D", "text": "1", "type": "forward"},
+    {"start": "D", "end": "D", "text": "0,1", "type": "self"}
   ],
   "input": [
     {
-      "string": "0101010",
-      "states": ["A", "B", "A", "B", "A", "B", "A", "B"]
+      "string": "010110",
+      "states": ["A", "A", "B", "C", "D", "D", "D"]
     },
     {
-      "string": "110100",
-      "states": ["A", "A", "A", "B", "A", "B", "B"]
+      "string": "1011111",
+      "states": ["A", "B", "C", "D", "D", "D", "D", "D"]
     },
     {
-      "string": "1001",
-      "states": ["A", "A", "B", "B", "A"]
+      "string": "001101111",
+      "states": ["A", "A", "A", "A", "B", "C", "D", "D", "D", "D"]
     }
   ]
 }
@@ -35,25 +36,22 @@ const nfa2 = {
     {"text": "C", "type": "accept"}
   ],
   "edges": [
-    {"start": "A", "end": "A", "text": "1", "type": "self"},
-    {"start": "A", "end": "B", "text": "0", "type": "forward"},
-    {"start": "B", "end": "C", "text": "1", "type": "forward"},
-    {"start": "B", "end": "B", "text": "0", "type": "self"},
-    {"start": "C", "end": "B", "text": "0", "type": "backward"},
-    {"start": "C", "end": "C", "text": "1", "type": "self"}
+    {"start": "A", "end": "A", "text": "0,1", "type": "self"},
+    {"start": "A", "end": "B", "text": "1", "type": "forward"},
+    {"start": "B", "end": "C", "text": "0,1", "type": "forward"}
   ],
   "input": [
     {
-      "string": "10011",
-      "states": ["A", "A", "B", "B", "C", "C"]
+      "string": "11010",
+      "states": ["A", "A", "A", "A", "B", "C"]
     },
     {
-      "string": "1010",
-      "states": ["A", "A", "B", "C", "B"]
+      "string": "1111",
+      "states": ["A", "A", "A", "B", "C"]
     },
     {
-      "string": "10101",
-      "states": ["A", "A", "B", "C", "B", "C"]
+      "string": "0011011",
+      "states": ["A", "A", "A", "A", "A", "A", "B", "C"]
     }
   ]
 }
@@ -63,27 +61,59 @@ const nfa3 = {
     {"text": "A", "type": "start"},
     {"text": "B", "type": "none"},
     {"text": "C", "type": "none"},
-    {"text": "D", "type": "accept"}
+    {"text": "D", "type": "none"},
+    {"text": "E", "type": "accept"}
   ],
   "edges": [
-    {"start": "A", "end": "A", "text": "1", "type": "self"},
-    {"start": "A", "end": "B", "text": "0", "type": "forward"},
+    {"start": "A", "end": "B", "text": "1", "type": "forward"},
+    {"start": "B", "end": "B", "text": "0,1", "type": "self"},
     {"start": "B", "end": "C", "text": "0", "type": "forward"},
     {"start": "C", "end": "D", "text": "0", "type": "forward"},
-    {"start": "D", "end": "D", "text": "0,1", "type": "self"}
+    {"start": "D", "end": "D", "text": "0,1", "type": "self"},
+    {"start": "D", "end": "E", "text": "1", "type": "forward"}
   ],
   "input": [
     {
-      "string": "1000",
-      "states": ["A", "A", "B", "C", "D"]
+      "string": "1001",
+      "states": ["A", "B", "C", "D", "E"]
     },
     {
-      "string": "00010",
-      "states": ["A", "B", "C", "D", "D", "D"]
+      "string": "100011",
+      "states": ["A", "B", "B", "C", "D", "D", "E"]
     },
     {
-      "string": "101",
-      "states": ["A", "A", "B", "B"]
+      "string": "11001",
+      "states": ["A", "B", "B", "C", "D", "E"]
     }
   ]
 }
+
+// const nfa3 = {
+//   "vertices": [
+//     {"text": "A", "type": "start"},
+//     {"text": "B", "type": "none"},
+//     {"text": "C", "type": "none"},
+//     {"text": "D", "type": "accept"}
+//   ],
+//   "edges": [
+//     {"start": "A", "end": "A", "text": "1", "type": "self"},
+//     {"start": "A", "end": "B", "text": "0", "type": "forward"},
+//     {"start": "B", "end": "C", "text": "0", "type": "forward"},
+//     {"start": "C", "end": "D", "text": "0", "type": "forward"},
+//     {"start": "D", "end": "D", "text": "0,1", "type": "self"}
+//   ],
+//   "input": [
+//     {
+//       "string": "1000",
+//       "states": ["A", "A", "B", "C", "D"]
+//     },
+//     {
+//       "string": "00010",
+//       "states": ["A", "B", "C", "D", "D", "D"]
+//     },
+//     {
+//       "string": "101",
+//       "states": ["A", "A", "B", "B"]
+//     }
+//   ]
+// }
